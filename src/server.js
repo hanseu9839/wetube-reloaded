fae60ff99ae881d46f428279e19e2d09b0cb2e1a
hanseu9839 <hanseu9839@gmail.com>
2022-04-07 17:21:27 +0900

    CROS error

--- src/server.js
+++ src/server.js
@@ -17,7 +17,11 @@ app.set("views",process.cwd() +"/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+    });
 app.use(
     session({
 	    secret:process.env.COOKIE_SECRET,
